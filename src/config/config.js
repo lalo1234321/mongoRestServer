264956da935f3fda483fcf3e998b8982852aa7e3
lalo1234321 <edominguezc@toluca.tecnm.mx>
2021-03-29 17:37:46 -0600

    Se configuro todo para el despliegue en la nube

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,13 @@
 process.env.PORT = process.env.PORT || 3001;
 
-process.env.URI_DB = 'mongodb://localhost:27017/tindog1';
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+if(process.env.NODE_ENV === 'dev'){
+    process.env.URI_DB = 'mongodb://localhost:27017/tindog1';
+} else {
+    process.env.URI_DB = 'mongodb+srv://lalo:lalo@cluster0.inmzb.mongodb.net/tindog?retryWrites=true&w=majority';
+}
+
+// process.env.URI_DB = 'mongodb+srv://lalo:lalo@cluster0.inmzb.mongodb.net/tindog?retryWrites=true&w=majority';
