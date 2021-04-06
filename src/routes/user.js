0e0cbbd73cdadbb36ef1fe7bac0795a9b21a4358
Jaki657 <postJK128@gmail.com>
2021-04-05 23:52:28 -0500

    se agrego el endpoint updateUser (tarea Jaqui)

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,23 +1,24 @@
-const {Router} = require('express');
+const { Router } = require('express');
 const router = Router();
 const User = require('../models/user.js');
 const mongo = require('mongoose');
+const { db } = require('../models/user.js');
 require('../config/config.js');
 
-router.get('/user',(req,res) => {
+router.get('/user', (req, res) => {
     res.status(200).json({
         message: 'hola mundo'
     });
 });
 
-router.post('/createUser', (req,res) => {
-    let {userName,password} = req.body;
+router.post('/createUser', (req, res) => {
+    let { userName, password } = req.body;
     let user = new User({
         userName,
         password
     });
     user.save((err, userDB) => {
-        if(err){
+        if (err) {
             return res.status(500).json({
                 err
             });
@@ -29,54 +30,67 @@ router.post('/createUser', (req,res) => {
     })
 });
 // Este endpoint lo realizó  abraham
-router.get('/getUser', (req, res) => 
-{
-    let arr=[];
-    mongo.connect(process.env.URI_DB, (err, db) =>{
-        if(!err)
-        {
-            const cursor = db.collection('users').find({status: true},{status:0});
-            cursor.forEach((doc, err)=>
-            {
-                if(!err)
-                {
+router.get('/getUser', (req, res) => {
+    let arr = [];
+    mongo.connect(process.env.URI_DB, (err, db) => {
+        if (!err) {
+            const cursor = db.collection('users').find({ status: true }, { status: 0 });
+            cursor.forEach((doc, err) => {
+                if (!err) {
                     arr.push(doc);
                 }
-            },() => 
-            {
-            //    db.close();
-               res.status(200).json({
-                   resultado: arr
-               });
+            }, () => {
+                //    db.close();
+                res.status(200).json({
+                    resultado: arr
+                });
             });
         }
     });
 });
 
-router.put('/updateUser',(req, res) => {
+/*router.put('/updateUser',(req, res) => {
     let {userName,password} = req.body;
     res.status(200).json({
         message:'modificación correcta',
         userName,
         password
     });
-})
+})*/
 
-
-router.delete('/deleteUser',(req, res) => {
-    let {userName,password} = req.body;
+router.put('/updateUser', (req, res) => {
+    let { idUser, userName, password } = req.body;
+    db.collection('users').updateOne(
+        { _id: { $eq: idUser } },
+        {
+            $set: {
+                userName,
+                password,
+                status: true,
+                _v: 0
+            }
+        },
+        { upsert: true });
     res.status(200).json({
-        message:'eliminación correcta',
+        message: 'Usuario modificado en la base de datos'
+    });
+});
+
+
+router.delete('/deleteUser', (req, res) => {
+    let { userName, password } = req.body;
+    res.status(200).json({
+        message: 'eliminación correcta',
         userName,
         password
     });
 })
 
 // este endpoint lo realizó lalo
-router.get('/getUsers', async(req,res) => {
-    const user = await User.find({status: true},{
+router.get('/getUsers', async (req, res) => {
+    const user = await User.find({ status: true }, {
         status: 0,
-        password:0
+        password: 0
     });
     res.status(200).json({
         result: user
